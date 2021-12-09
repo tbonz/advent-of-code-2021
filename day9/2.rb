588eb085f05cb3cfa25e85d3f7d6d7b2673ce784
Tyler <olson.tyler@gmail.com>
2021-12-08 23:57:21 -0600

    Add pt 2 file for day 9 with notes

---
+++ day9/2.rb
@@ -0,0 +1,43 @@
+class BasinFinder
+  def initialize(low_point)
+    @low_point = low_point
+  end
+
+  
+end
+
+things = File.read("input.txt")
+matrix = things.split.map{|row| row.split('').map(&:to_i)}
+
+low_points = []
+matrix.each_with_index do |row, row_index|
+  row.each_with_index do |point, col_index|
+    adjacent = []
+    begin
+      if row_index < (matrix.size - 1)
+        adjacent << matrix[row_index + 1][col_index] # down
+      end
+      if row_index > 0
+        adjacent << matrix[row_index - 1][col_index] # up
+      end
+      if col_index > 0
+        adjacent << matrix[row_index][col_index - 1] # left
+      end
+      if col_index < (row.size - 1)
+        adjacent << matrix[row_index][col_index + 1] # right
+      end
+    
+      if adjacent.min > point
+        low_points << point
+      end
+    rescue
+      require 'pry'; binding.pry
+    end
+  end
+end
+puts "Lows: #{low_points}"
+puts "Risk: #{low_points.map{|num| num + 1}.sum}"
+# require 'pry'; binding.pry
+
+# Think about taking each low point and intializing a BasinFinder class
+# This will need to understand how to crawl out from the low point and identify the basin
